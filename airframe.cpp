837cc4ed5eef3a458403ebabd4fdf15c36cc8970
Robb Walters <robb@senecameras.com>
2018-01-16 20:57:52 -0800

    prepare to prefer thrust to yaw when headroom is low

--- airframe.cpp
+++ airframe.cpp
@@ -8,14 +8,88 @@
 #include <Arduino.h>
 #include "controlVectors.h"
 
-uint16_t mix(const ControlVectors& controls, int32_t mFz, int32_t mTx, int32_t mTy, int32_t mTz) {
-    int32_t mmax = max(max(mFz, mTx), max(mTy, mTz));
-    return constrain((mFz * controls.force_z + mTx * controls.torque_x + mTy * controls.torque_y + mTz * controls.torque_z) / mmax, 0, 4095);
+void constrainPower(int32_t *desired_power, int32_t *available_power, int8_t* mix_table_values){
+    int32_t max_desired_power = 0;
+    size_t max_i = 0;
+    for (size_t i = 0; i < 8; ++i) {
+        if (desired_power[i] > max_desired_power ) {
+            max_desired_power = desired_power[i];
+            max_i = i;
+        }
+    }
+    if ( max_desired_power > available_power[max_i]){
+        int32_t scaled_deficit = ( max_desired_power - available_power[max_i] ) / ((int32_t) mix_table_values[max_i]);
+        for (size_t i = 0; i < 8; ++i) {
+            desired_power[i] -= scaled_deficit * (int32_t) mix_table_values[i];
+        }
+    }
 }
 
 void Airframe::setMotorsToMixTable(const ControlVectors& controls) {
+    const int32_t torque_reserve = 300;
+    int32_t remaining_power[8];
+    int32_t allocated_power[8];
+    int32_t fz_power[8];
+    int32_t tx_power[8];
+    int32_t ty_power[8];
+    int32_t tz_power[8];
+    for (size_t i = 0; i < 8; ++i) {
+        int32_t mmax = max(max(mix_table.fz[i], mix_table.tx[i]), max(mix_table.ty[i], mix_table.tz[i]));
+        fz_power[i] = mix_table.fz[i] * controls.force_z  / mmax;
+        tx_power[i] = mix_table.tx[i] * controls.torque_x / mmax;
+        ty_power[i] = mix_table.ty[i] * controls.torque_y / mmax;
+        tz_power[i] = mix_table.tz[i] * controls.torque_z / mmax;
+        remaining_power[i] = 4095 - torque_reserve;
+        allocated_power[i] = 0;
+    }
+
     for (size_t i = 0; i < 8; ++i)
-        motors_.set(i, mix(controls, mix_table.fz[i], mix_table.tx[i], mix_table.ty[i], mix_table.tz[i]));
+       motors_.set(i, (uint16_t) constrain(fz_power[i] + tx_power[i] + ty_power[i] + tz_power[i], 0, 4095));
+
+    /*  
+
+    // allocate thrust
+    for (size_t i = 0; i < 8; ++i) {
+        allocated_power[i] += min( remaining_power[i], fz_power[i] );
+        remaining_power[i] -= allocated_power[i];
+        
+        fz_power[i] -= allocated_power[i]; // keep track of residual thrust for later use
+        remaining_power[i] += torque_reserve; // add the power we held in reserve for torque back in
+    }
+    
+    // allocate roll
+    constrainPower(ty_power, remaining_power, mix_table.ty);
+    for (size_t i = 0; i < 8; ++i) {
+        allocated_power[i] += ty_power[i];
+        remaining_power[i] -= allocated_power[i];
+    }
+
+    // allocate pitch
+    constrainPower(tx_power, remaining_power, mix_table.tx);
+    for (size_t i = 0; i < 8; ++i) {
+        allocated_power[i] += tx_power[i];
+        remaining_power[i] -= allocated_power[i];
+    }
+
+    // allocate yaw
+    constrainPower(tz_power, remaining_power, mix_table.tz);
+    for (size_t i = 0; i < 8; ++i) {
+        allocated_power[i] += tz_power[i];
+        remaining_power[i] -= allocated_power[i];
+    }   
+
+    // if we have room, allocate the residual thrust
+    constrainPower(fz_power, remaining_power, mix_table.fz);
+    for (size_t i = 0; i < 8; ++i) {
+        allocated_power[i] += fz_power[i];
+        remaining_power[i] -= allocated_power[i];
+    }    
+
+    // set the levels
+    for (size_t i = 0; i < 8; ++i)
+        motors_.set(i, (uint16_t) constrain(allocated_power[i], 0, 4095));
+    
+    */
 }
 
 void Airframe::setMotor(size_t index, uint16_t value) {
