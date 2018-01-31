64d18ba7b69f6b58f1a66bd872d509d9718a69fa
Robb Walters <robb@senecameras.com>
2018-01-30 17:54:58 -0800

    use pilot override to test motors

--- testMode.cpp
+++ testMode.cpp
@@ -6,24 +6,25 @@
 
 #include "Arduino.h"
 #include "led.h"
-#include "motors.h"
 #include "state.h"
+#include "command.h"
+#include "controlVectors.h"
 
-void runTestRoutine(State& state, Motors& motors, size_t motor_id) {
-    for (auto& motorOut : state.MotorOut) {
-        motorOut = 0;
-    }
-    state.MotorOut[motor_id] = 4095;
-    motors.updateAllChannels();
+void runTestRoutine(State& state, LED& led, PilotCommand& pilot, size_t led_id, size_t motor_id) {
+    pilot.resetMotors();
+    pilot.setMotor(motor_id, 4095);
+    pilot.applyControl(ControlVectors());
+    led.setWhite(board::led::POSITION[led_id], board::led::POSITION[led_id], led_id % 2 == 0, led_id % 2 == 1);
+    led.update();
 }
 
-void runTestMode(State& state, LED& led, Motors& motors) {
-    state.set(STATUS_ENABLED);
+void runTestMode(State& state, LED& led, PilotCommand& pilot) {
+    pilot.override(true);
+    size_t led_id{0};
     size_t motor_id{0};
-    led.setWhite(board::led::Position::Min(), board::led::Position::Max(), true, true, 250);
-    led.update();
     while (true) {
-        runTestRoutine(state, motors, motor_id);
+        runTestRoutine(state, led, pilot, led_id, motor_id);
+        led_id = (led_id + 1) % 4;
         motor_id = (motor_id + 1) % 8;
         delay(500);
     }
